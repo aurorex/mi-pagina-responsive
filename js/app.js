675da88c9934e7c23ec7cd26501213ab9cf39973
aurora <selva_beat@hotmail.com>
2017-12-11 15:26:31 -0500

    modificando grilla

--- js/app.js
+++ js/app.js
@@ -1,18 +1,13 @@
 window.addEventListener('load', function() {
-    var icon = document.getElementById('icon');
-    var cont = ''
-    icon.addEventListener('click', function(event){
-        var menu = document.getElementById('ul-list');
-        
-        if (cont.value = 1) {
-            menu.setAttribute('class', 'ul-display');
-        }
-        else{
-            menu.removeAttribute('class', 'ul-display');
-        }
-        
-        
-       
-    })
+  var icon = document.getElementById('icon');
+  var cont = 0;
+  icon.addEventListener('click', function(event) {
+    var menu = document.getElementById('ul-list');
+    if (cont = 1) {
+      menu.setAttribute('class', 'ul-display');
+    } else {
+      menu.removeAttribute('class', 'ul-display');
+    }  
+  });
 
 })
